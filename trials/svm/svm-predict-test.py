bbe37dc129413aeb27b08a959d9ed6ed4624b844
Raghavendra <raghu.949@gmail.com>
2013-11-03 23:10:59 +0530

    Tried some models : 1. Basic excel based intuitive model 2. Basic python model 3. SVM

---
+++ trials/svm/svm-predict-test.py
@@ -0,0 +1,72 @@
+import csv as csv
+import numpy as np
+from sklearn import svm
+
+csv_file_object = csv.reader(open('train.csv', 'rb')) #Load in the csv file
+header = csv_file_object.next() #Skip the fist line as it is a header
+
+test_features = []
+train_features = []
+train_values = []
+temp = []
+
+maxage = 80.0
+maxsibsp = 8.0
+maxparch = 9.0
+
+for row in csv_file_object:
+	temp=[]
+	train_values.append(row[1])
+	temp.append(float(row[2])/3)
+	if row[4] is "male" :
+		temp.append(0)
+	else :
+		temp.append(1)
+	temp.append(float(row[5])/maxage)
+	temp.append(float(row[6])/maxsibsp)
+	temp.append(float(row[7])/maxparch)
+	if row[11] == 'S' :
+		temp.append(0.33)
+	elif row[11] == 'Q' :
+		temp.append(0.67)
+	else :
+		temp.append(0.99)
+	train_features.append(temp)
+
+csv_file_object = csv.reader(open('test.csv', 'rb')) #Load in the csv file
+header = csv_file_object.next() #Skip the fist line as it is a header
+
+for row in csv_file_object:
+	temp=[]
+	temp.append(float(row[1])/3)
+	if row[3] is "male" :
+		temp.append(0)
+	else :
+		temp.append(1)
+	temp.append(float(row[4])/maxage)
+	temp.append(float(row[5])/maxsibsp)
+	temp.append(float(row[6])/maxparch)
+	if row[10] == 'S' :
+		temp.append(0.33)
+	elif row[10] == 'Q' :
+		temp.append(0.67)
+	else :
+		temp.append(0.99)
+	test_features.append(temp)
+
+clf = svm.SVC(C=19.0, cache_size=2000, class_weight=None, coef0=10000, degree=6, gamma=150, kernel='rbf', max_iter=-1, probability=False, tol=0.001, verbose=True)
+clf.fit(train_features, train_values) 
+res = clf.predict(test_features)
+
+csv_file_object = csv.reader(open('test.csv', 'rb')) #Load in the csv file
+header = csv_file_object.next()
+
+predictions_file = csv.writer(open("svm-predict-test.csv", "wb"))
+predictions_file.writerow(["PassengerId", "Survived"])
+
+i=0
+correct_count=0
+
+for row in csv_file_object :
+	predictions_file.writerow([row[0],res[i]])
+	i=i+1
