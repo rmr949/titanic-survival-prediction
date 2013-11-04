af664b9b7bee5949fdc061869c4dda0ca935cc2e
Raghavendra <raghu.949@gmail.com>
2013-11-04 12:11:12 +0530

    Tried Neares neighbors model

---
+++ trials/nearestneighbours/nearestneighbours-predict-train.py
@@ -0,0 +1,98 @@
+import csv as csv
+import numpy as np
+from sklearn import neighbors
+
+csv_file_object = csv.reader(open('../../data/train-modified.csv', 'rb')) #Load in the csv file
+header = csv_file_object.next() #Skip the fist line as it is a header
+
+test_features = []
+train_features = []
+train_values = []
+temp = []
+
+maxage = 80.0
+maxsibsp = 8.0
+maxparch = 9.0
+
+n_neighbors = 7
+
+for row in csv_file_object:
+	temp=[]
+	train_values.append(row[1])
+	temp.append(float(row[2])/3)
+	if row[4] is "male" :
+		temp.append(0)
+	else :
+		temp.append(1)
+	temp.append(float(row[5])/maxage)
+	temp.append(float(row[6])/maxsibsp)
+	temp.append(float(row[7])/maxparch)
+	if row[11] == 'S' :
+		temp.append(0.33)
+	elif row[11] == 'Q' :
+		temp.append(0.67)
+	else :
+		temp.append(0.99)
+	train_features.append(temp)
+
+csv_file_object = csv.reader(open('../../data/test.csv', 'rb')) #Load in the csv file
+header = csv_file_object.next() #Skip the fist line as it is a header
+
+for row in csv_file_object:
+	temp=[]
+	temp.append(float(row[1])/3)
+	if row[3] is "male" :
+		temp.append(0)
+	else :
+		temp.append(1)
+	temp.append(float(row[4])/maxage)
+	temp.append(float(row[5])/maxsibsp)
+	temp.append(float(row[6])/maxparch)
+	# if row[10] == 'S' :
+	# 	temp.append(0.33)
+	# elif row[10] == 'Q' :
+	# 	temp.append(0.67)
+	# else :
+	# 	temp.append(0.99)
+	test_features.append(temp)
+
+
+for weights in ['uniform', 'distance']:
+    # we create an instance of Neighbours Classifier and fit the data.
+    clf = neighbors.KNeighborsClassifier(n_neighbors, weights='distance')
+    clf.fit(train_features, train_values)
+
+    # Plot the decision boundary. For that, we will assign a color to each
+    # point in the mesh [x_min, m_max]x[y_min, y_max].
+    # x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
+    # y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
+    # xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    res = clf.predict(train_features)
+
+
+# clf = svm.SVC(C=19.0, cache_size=2000, class_weight=None, coef0=10000, degree=6, gamma=10, kernel='rbf', max_iter=-1, probability=False, tol=0.001, verbose=True)
+# clf.fit(train_features, train_values) 
+# res = clf.predict(train_features)
+
+csv_file_object = csv.reader(open('../../data/train-modified.csv', 'rb')) #Load in the csv file
+header = csv_file_object.next()
+
+predictions_file = csv.writer(open("../../results/nn-train-v1.csv", "wb"))
+predictions_file.writerow(["PassengerId", "Survived"])
+
+i=0
+correct_count=0
+
+for row in csv_file_object :
+	predictions_file.writerow([row[0],res[i]])
+	print "res[i] = "+str(res[i])+"row[i] = "+str(row[1])
+	if res[i] == row[1] :
+		correct_count = correct_count + 1
+	i=i+1
+
+print i
+
+print "------------------------------------------------"
+print correct_count
+print "Accuracy : "+str(float(correct_count)/i)
+print "------------------------------------------------"
